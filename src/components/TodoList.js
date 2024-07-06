64db88c02f0a4a64c9658566b8f24a6212c107b6
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-06 15:40:48 +0530

    fourth commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 const TodoList = ({ todos, handleComplete }) => {
+
   return (
     <>
     <h2>Child Component</h2>
@@ -9,7 +9,7 @@ const TodoList = ({ todos, handleComplete }) => {
         <li key={todo.id}>
           {todo.text}
           {!todo.completed && (
-            <button onClick={() => handleComplete(todo.id)}>complete</button>
+            <button onClick={() => handleComplete(todo.id)}>Complete</button>
           )}
         </li>
       ))}
